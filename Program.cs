3a62ed36f0e4f3a2183c217f3c99859929b14301
Ambrož Janc <ambrozj@gen-i.si>
2023-12-04 06:48:57 +0100

    Day 4 done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using AdventOfCode2023.Tasks;
 
 string filename = $"D:\\GeniRepos\\AdventOfCode2023\\Inputs\\1.txt";
-var task = new Task3();
+var task = new Task4();
 
-//task.Solve1(File.ReadAllText(task.Filename));
+//ask.Solve1(File.ReadAllText(task.Filename));
 task.Solve2(File.ReadAllText(task.Filename));
---
+++ Tasks/Task4.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Tasks
+{
+    public class Task4 : AdventTask
+    {
+        public Task4()
+        {
+            Filename += "4.txt";
+        }
+
+        public override void Solve1(string input)
+        {
+            var lines = GetLinesList(input);
+            double result = 0;
+            foreach(var line in lines)
+            {
+                var splitted = line.Split(':');
+                var nums = splitted[1].Split("|");
+                var winning = nums[0].Split(" ").ToList();
+                var my = nums[1].Split(" ").ToList();
+                winning = winning.Select(w => w.Trim()).Where(w => w != "").ToList();
+                my = my.Select(w => w.Trim()).Where(w => w != "").ToList();
+                var won = winning.Intersect(my).ToList();
+                if (won.Count > 0) 
+                {
+                    result += Math.Pow(2, won.Count - 1);
+                }
+            }
+            Console.WriteLine(result);
+        }
+
+        public override void Solve2(string input)
+        {
+            var lines = GetLinesList(input);
+            double result = 0;
+            var copies = new int[lines.Count];
+            for(int k = 0; k < lines.Count; k++) { copies[k] = 0; }
+            int i = 0;
+            foreach (var line in lines)
+            {
+                var splitted = line.Trim().Split(':');
+                var nums = splitted[1].Split("|");
+                var winning = nums[0].Split(" ").ToList();
+                var my = nums[1].Split(" ").ToList();
+                winning = winning.Where(w => w != "").ToList();
+                my = my.Where(w => w != "").ToList();
+                var won = winning.Intersect(my).ToList();
+                result += 1 + copies[i];
+                for (int k = 0; k < copies[i] + 1; k++)
+                {
+                    for (int j = 1; j < won.Count + 1; j++)
+                    {
+                        copies[i + j] += 1;
+                    }
+                }
+                i++;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
