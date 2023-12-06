be3cda8b8e563695c2eba38ad5a191f2129ac32d
Ambrož Janc <ambrozj@gen-i.si>
2023-12-06 06:48:27 +0100

    Day 6 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿using AdventOfCode2023.Tasks;
 
 string filename = $"D:\\GeniRepos\\AdventOfCode2023\\Inputs\\1.txt";
-var task = new Task5();
+var task = new Task6();
+var task1Watch = new System.Diagnostics.Stopwatch();
+var task2Watch = new System.Diagnostics.Stopwatch();
 
+Console.WriteLine("Solving First Task:");
+task1Watch.Start();
 task.Solve1(File.ReadAllText(task.Filename));
+task1Watch.Stop();
+Console.WriteLine($"Execution Time: {task1Watch.ElapsedMilliseconds} ms");
+
+Console.WriteLine("Solving Second Task:");
+task2Watch.Start();
 task.Solve2(File.ReadAllText(task.Filename));
+task2Watch.Stop();
+Console.WriteLine($"Execution Time: {task2Watch.ElapsedMilliseconds} ms");
---
+++ Tasks/Task6.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Tasks
+{
+    public class Task6 : AdventTask
+    {
+        public Task6()
+        {
+            Filename += "6.txt";
+        }
+
+        public override void Solve1(string input)
+        {
+            var lines = GetLinesList(input);
+            long result = 1;
+            var times = GetSplittedLine(lines, 0);
+            var distances = GetSplittedLine(lines, 1);
+            for (int i = 0; i < times.Count(); i++)
+            {
+                var time = int.Parse(times.ElementAt(i).Trim());
+                var dist = int.Parse(distances.ElementAt(i).Trim());
+                var winningSituations = 0;
+                var isEven = time % 2 == 0;
+                var timeToAdd = isEven ? 0 : 1;
+                for (long speed = 1; speed < time / 2 + timeToAdd; speed++)
+                {
+                    winningSituations += CheckIfWinning(speed, time, dist);
+                }
+                winningSituations *= 2;
+                if (isEven)
+                {
+                    long speed = time / 2;
+                    winningSituations += CheckIfWinning(speed, time, dist);
+                }
+                if (winningSituations > 0)
+                    result *= winningSituations;
+            }
+            Console.WriteLine(result);
+        }
+
+        public override void Solve2(string input)
+        {
+            var lines = GetLinesList(input); 
+            long result = 1;
+            var times = GetSplittedLine(lines, 0);
+            var realTimes = string.Join("", times);
+            var distances = GetSplittedLine(lines, 1);
+            var realDistances = string.Join("", distances);
+            var time = long.Parse(realTimes.Trim());
+            var dist = long.Parse(realDistances.Trim());
+            int winningSituations = 0;
+            for (int speed = 1; speed < time; speed++)
+            {
+                winningSituations += CheckIfWinning(speed, time, dist);
+            }
+            if (winningSituations > 0)
+                result *= winningSituations;
+
+            Console.WriteLine(result);
+        }
+
+        private List<string> GetSplittedLine(List<string> lines, int lineIndex)
+        {
+            return lines.ElementAt(lineIndex).Split(" ").Where(d => d != "").Skip(1).ToList();
+        }
+
+        private int CheckIfWinning(long speed, long time, long dist)
+        {
+            var remainingTime = time - speed;
+            var travelled = speed * remainingTime;
+            if (travelled > dist)
+                return 1;
+            return 0;
+        }
+    }
+}
